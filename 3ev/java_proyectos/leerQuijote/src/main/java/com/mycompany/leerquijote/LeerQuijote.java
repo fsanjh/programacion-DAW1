41751f4d2cb16f70bdca728e1d712971c1e94a20
fsanj <fsanjosehernan@hotmail.com>
2024-04-24 19:04:47 +0200

    quijote 1.0

--- 3ev/java_proyectos/leerQuijote/src/main/java/com/mycompany/leerquijote/LeerQuijote.java
+++ 3ev/java_proyectos/leerQuijote/src/main/java/com/mycompany/leerquijote/LeerQuijote.java
@@ -11,27 +11,47 @@ import java.io.IOException;
 public class LeerQuijote{
 
     public static void main(String[] args){
+        //Contadores de palabras
         int contDulcinea = 0;
+        int contQuijote = 0;
+        int contSancho = 0;
+        int contRocinante = 0;
+        String palabra = ""; //Cada palabra completa
+        //Ubicación del fichero
         String path = "C:\\Users\\Nocturno\\pruebasEscrituraFicheros\\";
 
         try{
-
+            //Declaro el FileReader añadiendo en el constructor el path y el nobre del fichero
             FileReader fr = new FileReader(path + "quijote.txt");
-            BufferedReader br = new BufferedReader(fr);
-            int letra;
-            String palabra = "";
-            while((letra = br.read()) != -1){
-                if(!(Character.isWhitespace(letra)) || Character.isAlphabetic(letra) ){
-                    palabra = palabra + (char)letra;
-                }else{
-                    if(palabra.equals("Dulcinea")){
-                        System.out.println(palabra);
-                        contDulcinea++;
+            BufferedReader br = new BufferedReader(fr); //Declaro el BufferedReader
+            int caracter; //caracter que se guardara
+            
+            while((caracter = br.read()) != -1){ //Bucle que leera el fichero
+                //Si el caracter actual es una letra
+                if(!Character.isWhitespace(caracter) && Character.isAlphabetic(caracter)){ 
+                    palabra = palabra + (char)caracter; //Concateno el caracter con la palabra
+                }else{ //Cuando el caracter es un espacio, coma, punto...
+                    palabra = palabra.toLowerCase(); //para que todas coincidan
+                    //Si la palabra empieza por "dulcinea" y termina con cualquier cosa (dulcineaS, dulcineaE...)
+                    if(palabra.startsWith("dulcinea")){ 
+                        contDulcinea++; //Sumo el contador
+                    }else if(palabra.startsWith("quijote")){
+                        contQuijote++;
+                    }else if(palabra.startsWith("sancho")){
+                        contSancho++;
+                    }else if(palabra.startsWith("rocinante")){
+                        contRocinante++;
                     }
-                    palabra = "";
+                    palabra = ""; //limpio la palabra para volver a buscar
                 }
             }
-            System.out.println(contDulcinea);
+            br.close();
+            fr.close();
+            //Muestro pot pantalla la cantidad
+            System.out.println("Dulcinea: "+ contDulcinea); 
+            System.out.println("Quijote: "+ contQuijote);
+            System.out.println("Sancho: "+ contSancho);
+            System.out.println("Rocinante: "+ contRocinante);
 
         }catch(IOException ioex){
             System.out.println(ioex.getMessage());
